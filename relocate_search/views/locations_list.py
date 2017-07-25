f384d3d8769d4aa937dd165d9f6e7843910c2349
Boomerang <boomerang@trafficstars.com>
2017-07-25 14:13:44 +0300

    added search on map

--- relocate_search/views/locations_list.py
+++ relocate_search/views/locations_list.py
@@ -1,8 +1,18 @@
+from django.db.models import Q
 from django.views.generic import ListView
 
-from relocate_search.models import JobLocation
+from relocate_search.models import JobLocation, Job
 
 
 class LocationsList(ListView):
     model = JobLocation
     template_name = 'locations_list.html'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(LocationsList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('search'):
+            search = self.request.GET.get('search')
+            jobs = Job.objects.filter(Q(name__icontains=search)|Q(tags__name__iexact=search.lower()))
+            job_locations = set(jobs.values_list('location_id', flat=True))
+            queryset = queryset.filter(pk__in=job_locations)
+        return queryset
